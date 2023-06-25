cb6ba6fe54b373df1c2d4041cfe5ab8e1778c999
Ahmad Damra <ahmeddamra_cs@hotmail.com>
2023-06-25 15:37:01 +0300

    s

---
+++ OnlineTestApp/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineTestApp.Models;
+using System.Diagnostics;
+
+namespace OnlineTestApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            // ahmad damra
+            return View();
+        }
+        public IActionResult Index1()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
